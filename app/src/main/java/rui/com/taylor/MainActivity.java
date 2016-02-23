87e87de986ffb8836febd2adc8289ad53c76c6e0
lunarwolves <lunarwolves@163.com>
2016-02-23 08:51:09 +0800

    Test commit

--- app/src/main/java/rui/com/taylor/MainActivity.java
+++ app/src/main/java/rui/com/taylor/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+ * Create first activity
+ */
 public class MainActivity extends AppCompatActivity {
 
 	@Override
